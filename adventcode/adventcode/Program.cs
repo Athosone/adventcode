c06a0bad7cec0d3ff9da1021c703ff934b15314f
Athosone <werckayrton@gmail.com>
2015-12-11 01:41:36 +0100

    Two days done
    The code of the two day is horrible to read but I wanted to do it with the minimum of lines and I wanted to use Linq

---
+++ adventcode/adventcode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace adventcode
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Jour1
+			Console.WriteLine("Result Jour1 part1: " + Jour1.GetResult ());
+			Console.WriteLine("Result Jour1 part2: " + Jour1.GetResultPart2 ());
+
+			//Jour2
+			Console.WriteLine("Result Jour2 part1: " + Jour2.GetResultPart1 ());
+			Console.WriteLine("Result Jour2 part2: " + Jour2.GetResultPart2 ());
+
+
+		}
+	}
+}
