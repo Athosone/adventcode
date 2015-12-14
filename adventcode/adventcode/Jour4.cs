4d929eaee31a2c6dd2a4b15b1c9284532af82300
Athosone <werckayrton@gmail.com>
2015-12-14 20:46:08 +0100

    Jour 4 done

--- adventcode/adventcode/Jour4.cs
+++ adventcode/adventcode/Jour4.cs
@@ -1,15 +1,55 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
 
 namespace adventcode
 {
+
+	//#MultiThread
 	public class Jour4
 	{
+		//private static volatile Object locker = new Object ();
+		private static volatile MD5 md5 = System.Security.Cryptography.MD5.Create ();
+
+		public static string input = "yzbqklnj";
+
 		public Jour4 ()
 		{
 		}
 
 
-		//public static string input "";
+		public static string CreateMD5 (string input)
+		{
+			byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes (input);
+			byte[] hashBytes = null;
+
+			//lock (locker) {
+				hashBytes = md5.ComputeHash (inputBytes);
+			//}
+
+			StringBuilder sb = new StringBuilder ();
+			for (int i = 0; i < hashBytes.Length; i++) {
+				sb.Append (hashBytes [i].ToString ("X2"));
+			}
+			return sb.ToString ();
+		}
+			
+		public static int GetResult (int range, string match)
+		{
+			int position = 0;
+			string testString = "         ";
+
+			while (testString.Substring (0, range) != match) {
+				testString = CreateMD5 (input + position);
+				position = position + 1;
+			}
+			return position - 1;
+		}
 	}
 }
 
--- adventcode/adventcode/Program.cs
+++ adventcode/adventcode/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace adventcode
 {
@@ -18,6 +21,32 @@ namespace adventcode
 			Console.WriteLine("Result Jour3 part1: " + Jour3.GetResultPart1 ());
 			Console.WriteLine("Result Jour3 part2: " + Jour3.GetResultPart2 ());
 
+			//Jour4
+
+			Task j4Part1 = Task.Run (() => {
+
+				Stopwatch sw = Stopwatch.StartNew();
+				Console.WriteLine("Start j4Part1");
+				int result =  Jour4.GetResult(5, "00000");
+				Console.WriteLine("Result Jour4 part1: " + result);
+				sw.Stop();
+				Console.WriteLine("Time taken J4 Part1: {0} s", sw.Elapsed.TotalSeconds);
+
+			});
+
+			Task j4Part2 =  Task.Run (() => {
+
+				Stopwatch sw = Stopwatch.StartNew();
+				Console.WriteLine("Start j4Part2");
+				int result2 =  Jour4.GetResult(6, "000000");
+				Console.WriteLine("Result Jour4 part2: " + result2);
+				sw.Stop();
+				Console.WriteLine("Time taken J4 Part1: {0} s", sw.Elapsed.TotalSeconds);
+
+			});
+			j4Part1.Wait ();
+			j4Part2.Wait ();
+			Console.WriteLine ("Done");
 		}
 	}
 }
